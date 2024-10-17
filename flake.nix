c243a27799b08abdc77cf3a2e7fa5e3514948992
Plasny <git.plasny.uq95y@slmail.me>
2024-10-17 20:26:06 +0200

    added quality of life stuff

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          python313
+          python313Packages.pip
+          python313Packages.virtualenv
+        ];
+      
+        shellHook = ''
+          echo "Welcome to development shell with Python 13"
+        '';
+      };
+  });
+}
+
